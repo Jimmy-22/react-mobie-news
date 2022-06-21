1db50e3c8989ebdb4d3b0fcc944a7256629975b9
ferrytsui <ferrytsui@163.com>
2022-06-21 18:00:31 +0800

    feat：tabBar的基本渲染

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,5 +1,28 @@
-const Layout = ()=>{
-  return <div>这是布局组件</div>
+import styles from './index.module.scss'
+import { TabBar } from 'antd-mobile'
+import Icon from '@/components/Icon'
+
+const Layout = () => {
+  const tabs = [
+    { path: '/', icon: 'iconbtn_home', text: '首页' },
+    { path: '/profile', icon: 'iconbtn_mine', text: '我的' }
+  ]
+
+  return (
+    <div className={styles.root}>
+      <TabBar className="tab-bar">
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            icon={(active) => (
+              <Icon type={active ? `${item.icon}_sel` : item.icon} />
+            )}
+            title={item.text}
+          />
+        ))}
+      </TabBar>
+    </div>
+  )
 }
 
 export default Layout
