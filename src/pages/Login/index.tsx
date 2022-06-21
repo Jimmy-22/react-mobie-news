5fe726f7703eb74a8a49d43c7f9cb6f4ac59c7b3
ferrytsui <ferrytsui@163.com>
2022-06-21 10:31:13 +0800

    feat：登录表单基本实现

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,35 @@
-import { Button } from 'antd-mobile'
-import Icon from '../../components/Icon'
+import { NavBar, Form, Input, List, Button } from 'antd-mobile'
+import styles from './index.module.scss'
 
-const Login = () => (
-  <div>
-    <Icon
-      type="iconbtn_like_sel"
-      onClick={() => {
-        console.log('图标事件触发')
-      }}
-    />
-    <Button color="primary">按钮</Button>
-  </div>
-)
+const Login = () => {
+  return (
+    <div className={styles.root}>
+      <NavBar></NavBar>
+      <div className="login-form">
+        <h2 className="title">账号登录</h2>
+        <Form>
+          <Form.Item name="mobie" className="login-item">
+            <Input placeholder="请输入姓名" />
+          </Form.Item>
+          <List.Item className="login-code-extra">
+            <Form.Item name="code" className="login-item">
+              <Input placeholder="请输入验证码" autoComplete="off"></Input>
+            </Form.Item>
+          </List.Item>
+          <Form.Item noStyle>
+            <Button
+              block
+              type="submit"
+              color="primary"
+              className="login-submit"
+            >
+              提交
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
+}
 
 export default Login
