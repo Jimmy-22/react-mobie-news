bee3655ef06b587df1ddd18c1998ff46c0aab1d2
ferrytsui <ferrytsui@163.com>
2022-06-22 09:44:35 +0800

    feat：路由的切换，及刷新保持激活tab

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Layout from './pages/Layout'
 import ClassNames from './pages/ClassNames'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -9,7 +11,10 @@ const App = () => {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
           <Route path="/test" element={<ClassNames />}></Route>
         </Routes>
       </div>
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,16 +1,27 @@
 import styles from './index.module.scss'
 import { TabBar } from 'antd-mobile'
 import Icon from '@/components/Icon'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 const Layout = () => {
   const tabs = [
     { path: '/', icon: 'iconbtn_home', text: '首页' },
     { path: '/profile', icon: 'iconbtn_mine', text: '我的' }
   ]
+  const navigate = useNavigate()
+  const tabChange = (key: string) => {
+    navigate(key)
+  }
+  const location = useLocation()
 
   return (
     <div className={styles.root}>
-      <TabBar className="tab-bar">
+      <Outlet />
+      <TabBar
+        className="tab-bar"
+        onChange={tabChange}
+        activeKey={location.pathname}
+      >
         {tabs.map((item) => (
           <TabBar.Item
             key={item.path}
